85fa0c94197b8c218f9e41323f6bf5fb2463364d
fjose <frankjose1992@gmail.com>
2022-10-04 21:15:58 -0500

    Inital Commit

---
+++ src/main/java/app/repository/ProveedorRepository.java
@@ -0,0 +1,7 @@
+package app.repository;
+
+import app.entity.ProveedorEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProveedorRepository extends JpaRepository<ProveedorEntity, Long> {
+}
