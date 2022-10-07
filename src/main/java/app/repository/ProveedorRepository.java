7f12d03a4f7d4cbead4beda32f09211f1d3b4f15
fjose <frankjose1992@gmail.com>
2022-10-06 19:53:51 -0500

    test..

--- src/main/java/app/repository/ProveedorRepository.java
+++ src/main/java/app/repository/ProveedorRepository.java
@@ -4,4 +4,5 @@ import app.entity.ProveedorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProveedorRepository extends JpaRepository<ProveedorEntity, Long> {
+
 }
